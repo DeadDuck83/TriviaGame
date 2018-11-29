40bd232a5a1b531d61f0d3635e474ed6b28930e8
Derek Moore <moore8577@gmail.com>
2018-11-28 22:40:27 -0800

    questions written. Static HTML loaded.

---
+++ asseets/js/app.js
@@ -0,0 +1,87 @@
+var questions = [
+    {
+        question: "In what place was Christmas once illegal?",
+        opt1: "Russia",
+        opt2: "Brazil",
+        opt3: "England",
+        opt4: "France",
+        answer: "England"
+    },
+    {
+        question: "In California, it is illegal to eat oranges while doing what?",
+        opt1: "Bathing",
+        opt2: "Driving",
+        opt3: "Gardening",
+        opt4: "Working on a computer",
+        answer: "Bathing"
+    },
+    {
+        question: "Coulrophobia means fear of what?",
+        opt1: "Sacred Things",
+        opt2: "Clowns",
+        opt3: "Old People",
+        opt4: "Jews",
+        answer: "Clowns"
+    },
+    {
+        question: "How many dimples are there on a regular golf ball?",
+        opt1: "418",
+        opt2: "336",
+        opt3: "294",
+        opt4: "377",
+        answer: "336"
+    },
+    {
+        question: "Which of the following is the longest running American animated TV show?",
+        opt1: "Rugrats",
+        opt2: "TV Funhouse",
+        opt3: "Pokemon",
+        opt4: "Simpsons",
+        answer: "Simpsons"
+    },
+    {
+        question: "Every year, over 8,800 people injure themselves with what apparently harmless, tiny object?",
+        opt1: "Knife",
+        opt2: "Toothpick",
+        opt3: "Baseball Bat",
+        opt4: "Pencil",
+        answer: "Toothpick"
+    },
+    {
+        question: "How many pounds of pressure do you need to rip off your ear?",
+        opt1: "11",
+        opt2: "17",
+        opt3: "7",
+        opt4: "2",
+        answer: "7"
+    },
+    {
+        question: "At what temperature are Fahrenheit and Celsius the same?",
+        opt1: "-40",
+        opt2: "50",
+        opt3: "0",
+        opt4: "92",
+        answer: "-40"
+    },
+    {
+        question: "What are the odds of being killed by space debris?",
+        opt1: "1 in 5 million",
+        opt2: "1 in 5 billion",
+        opt3: "1 in 10 billion",
+        opt4: "1 in 1 trillion",
+        answer: "1 in 5 billion"
+    },
+    {
+        question: "How much does Earth weigh?",
+        opt1: "6,588,000,000,000,000,000,000,000,000 tons",
+        opt2: "26,877,000,000,000,000,000,000,000,000,000,000,000,000 tons",
+        opt3: "10,000,000,000,000,000,000,000,000 tons",
+        opt4: "5 pounds",
+        answer: "6,588,000,000,000,000,000,000,000,000 tons"
+    }
+];
+console.log(questions);
+
+// print question and options to HTML
+
+//
